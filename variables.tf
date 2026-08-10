825e7d540c8f4494187389857dba586e7761f87b
Vikas Tiwari <vikast@VikasT-Mac.local>
2026-08-11 01:54:56 +0530

    Initialize Terraform AWS provider configuration

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region used to deploy infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
